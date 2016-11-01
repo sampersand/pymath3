b0788196ce64d155ab5589a12a738140e1a978d2
Sam <sam@cwc-0178.dhcp.wustl.edu>
2016-11-01 14:10:22 -0500

    I think i'll make a doc meta class... haha

--- builtins/core/math_obj.py
+++ builtins/core/math_obj.py
@@ -1,24 +1,37 @@
-from . import logger
+# from . import logger
 class MathObj():
 	'''
 	Base class for all PyMath Objects.
 
-	A warning will be logged if a ValuedObj is attempted to be instanced directly.
+	A warning will be logged if a MathObj is attempted to be instanced directly.
 	'''
+
+	__doc_defaults__ = {
+		'__init__': '''
+			Instantiates self.
+
+			A warning will be logged if 'type(self) == {ctype}' evaluates to true.
+
+			Arguments:
+				*args    -- Ignored{kwonly_args}
+				**kwargs -- Ignored
+			Returns:
+				None
+			''',
+		'__repr__': '''No doc lol noob
+		''',
+	}
+
+
+
+
+
 	def __init__(self, *args, **kwargs):
-		''' Instantiates self.
-
-		A warning will be logged if a MathObj is attempted to be instanced directly.
-
-		Arguments:
-			**kwargs -- Extra kwargs, will be ignored for this class.
-		Returns:
-			None
-		'''
-
 		if __debug__ and type(self) == MathObj:
 			logger.warning("Should not instantiate {} directly!".format(type(self).__qualname__))
 		super().__init__(*args, **kwargs)
 
 	def __repr__(self):
 		return '{}()'.format(type(self).__qualname__)
+
+
